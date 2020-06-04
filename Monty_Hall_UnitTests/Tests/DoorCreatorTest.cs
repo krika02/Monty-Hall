a1ed41e5f2f108a4c3687b1e40d0881ed49fefff
Kristoffer Karlsson <hello@kristofferkarlsson.me>
2020-06-04 15:35:04 +0200

    Restructure

---
+++ Monty_Hall_UnitTests/Tests/DoorCreatorTest.cs
@@ -0,0 +1,42 @@
+﻿using Monty_Hall.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monty_Hall_UnitTests.Tests
+{
+    public class DoorCreatorTest
+    {
+        private IDoorCreator _doorCreator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _doorCreator = new DoorCreator();
+        }
+
+        /// <summary>
+        /// Ensure that there are 3 numbers of doors
+        /// </summary>
+        [Test]
+        public void Doors_Correct_Number()
+        {
+            var doors = new DoorCreator().GenerateRandomDoors();
+
+            Assert.IsTrue(doors.Count == 3);
+        }
+
+        /// <summary>
+        /// Ensure that 1 door is the winning option
+        /// </summary>
+        [Test]
+        public void Doors_One_Winner()
+        {
+            var doors = _doorCreator.GenerateRandomDoors();
+
+            Assert.IsTrue(doors.Count(n => n.WinningOption == true) == 1);
+        }
+    }
+}
---
+++ Monty_Hall_UnitTests/Tests/MontyHallCalculatorTest.cs
@@ -0,0 +1,39 @@
+﻿using Monty_Hall.Services;
+using Monty_Hall.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monty_Hall_UnitTests.Tests
+{
+    public class MontyHallCalculatorTest
+    {
+        [TestCase(1, true, true)]
+        [TestCase(2, false, true)]
+        [TestCase(3, true, true)]
+        [TestCase(1, false, false)]
+        [TestCase(2, true, false)]
+        public void Calculation_Correct(int doorNumber, bool changeDoor, bool expectedResult)
+        {
+            // Calculate will call GenerateRandomDoors so we will mock that function to always return the same result
+
+            var doorCreatorMock = new Mock<IDoorCreator>();
+            var doors = new List<DoorModel>()
+            {
+                new DoorModel(){ DoorNumber = 1, WinningOption = false},
+                new DoorModel(){ DoorNumber = 2, WinningOption = true},
+                new DoorModel(){ DoorNumber = 3, WinningOption = false}
+            };
+            doorCreatorMock.Setup(n => n.GenerateRandomDoors()).Returns(doors);
+
+            var calculator = new MontyHallCalculator(doorCreatorMock.Object);
+
+            var result = calculator.Calculate(doorNumber, changeDoor);
+
+            Assert.IsTrue(result == expectedResult);
+        }
+    }
+}
